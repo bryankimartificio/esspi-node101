60d0acb272d6ab4937eb41f37fc97a86fc797340
bryankim.artificio@essilor.com <bryankim.artificio@essilor.com>
2019-09-25 22:59:52 +0800

    T4

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // x-www-form-urlencoded 
-app.post('/v1/consumer/', (req, res) => res.status(200).send(`Username: ${req.body.username}`));
+app.post('/v1/consumer/', (req, res) => res.status(500).send(`Username: ${req.body.username}`));
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
